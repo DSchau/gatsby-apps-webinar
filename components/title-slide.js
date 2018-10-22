75a91a01ee5f183d0a77bec112d7c94c01c6a37d
Dustin Schau <dustinschau@gmail.com>
2018-10-22 18:18:52 -0500

    chore: tweak title slide

---
+++ components/title-slide.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Container = styled.div([], {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  width: '100vw'
+})
+
+const Section = styled.div([], {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  alignItems: 'flex-start',
+  justifyContent: 'flex-end',
+  paddingLeft: '10vw',
+  paddingBottom: '5vh',
+  paddingTop: '5vh',
+  'h1, h2, h3, h4, h5, h6': {
+    color: '#663399',
+    textAlign: 'left',
+    margin: 0
+  }
+}, props => props.secondary ? css({
+  justifyContent: 'flex-start',
+  backgroundColor: `#663399`,
+  'h1, h2, h3, h4, h5, h6': {
+    color: 'white'
+  }
+}) : null)
+
+function TitleSlide({ children }) {
+  const [first, second = <React.Fragment />] = React.Children.toArray(children);
+  return (
+    <Container>
+      <Section>{first}</Section>
+      <Section backgroundColor="#" secondary={true}>{second}</Section>
+    </Container>
+  );
+}
+
+export default TitleSlide
